8431a2bbf708287b8d0b01508fd501c8dd803803
Jason Charney <jrcharney@gmail.com>
2020-09-28 14:01:49 -0500

    This too

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,19 @@ import { Routes, RouterModule } from '@angular/router';
 // Remember: Components are generated using `ng generate component comp_name`.
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
+import { TosComponent} from './tos/tos.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+
 
 const routes: Routes = [
-  { path : 'home', component: HomeComponent },
-  { path : 'profile', component : ProfileComponent }
+  { path : 'home', component: HomeComponent },        // Home Page
+  { path : 'profile', component : ProfileComponent }, // User Profile Page
+  { path : 'help', component : HelpComponent },       // Help Page
+  { path : 'about', component : AboutComponent },     // About Page
+  { path : 'tos', component : TosComponent },         // Terms of Service
+  { path : 'disclaimer', component : DisclaimerComponent },  // Disclaimer Page
 ];
 
 @NgModule({
