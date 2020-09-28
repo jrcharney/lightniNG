66190f0202c55744f4435c5b905699aadbda72a9
Jason Charney <jrcharney@gmail.com>
2020-09-28 14:01:05 -0500

    Well, I got to push something

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Because we are using routing we need to define our pages as components
+// Remember: Components are generated using `ng generate component comp_name`.
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  { path : 'home', component: HomeComponent },
+  { path : 'profile', component : ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { TosComponent } from './tos/tos.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { HelpComponent } from './help/help.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    HomeComponent,
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+    AboutComponent,
+    TosComponent,
+    DisclaimerComponent,
+    HelpComponent,
+    ForgotComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
